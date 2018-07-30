74b676f2592bf80fc543b302848619599853923c
malek <malek@LAPTOP-28CEC0C1>
2018-07-30 12:18:10 +0100

    mmb
    Signed-off-by: malek <malek@LAPTOP-28CEC0C1>

--- src/main/java/com/example/demo/controller/AgentController.java
+++ src/main/java/com/example/demo/controller/AgentController.java
@@ -19,18 +19,18 @@ import com.example.demo.Entities.Agent;
 import com.example.demo.Entities.Planning;
 import com.example.demo.Entities.Service;
 import com.example.demo.Entities.Site;
-import com.example.demo.repository.Agentrepository;
+import com.example.demo.repository.AgentRepository;
 import com.example.demo.repository.SiteRepository;
 @CrossOrigin("*") 
 @RestController
 public class AgentController {
 @Autowired
-Agentrepository r;
+AgentRepository r;
 @Autowired
 SiteRepository site;
 @GetMapping("/agent/afficher/{id}")
 
-public List<Agent> afficheragent(@PathVariable(value="id") Long id)
+public List<Agent> afficheListAgentBySiteId(@PathVariable(value="id") Long id)
 {
 int i,j;	
 	List<Agent>a = new ArrayList<>();
@@ -45,6 +45,27 @@ for(i=0;i<ser.size();i++)
 	}
 return a;
 	}
+@GetMapping("/agent/recherche/{nom}/{id}")
+public List<Agent> rechercheagent(@PathVariable(value="nom")String nom,@PathVariable(value="id") Long id)
+{List<Agent> b=new ArrayList<>();
+int i,j;	
+List<Agent>a = new ArrayList<>();
+Site s=site.affliste(id);
+Planning p=s.getPlanning();
+List<Service>ser=p.getServices();
+for(i=0;i<ser.size();i++)
+{
+for(j=0;j<ser.get(i).getAgents().size();j++)
+{
+	a.add(ser.get(i).getAgents().get(j));	}
+}
+for(i=0;i<a.size();i++)
+{
+	if(a.get(i).getNom().equals(nom))
+	{b.add(a.get(i));}
+}
+return b;
+}
 @GetMapping("/agent/affichersites")
 public List<Site> affichersite()
 {
--- src/main/java/com/example/demo/repository/AgentRepository.java
+++ src/main/java/com/example/demo/repository/AgentRepository.java
@@ -1,11 +1,9 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.Entities.Agent;
 
-public interface Agentrepository extends JpaRepository<Agent,Long> {
-	
+public interface AgentRepository extends JpaRepository<Agent, Long> {
 
 }
