500b63cdbf9bbbc72fe43c5d838cf58a4140c046
firas saadaoui <firas.saadaoui@esprit.tn>
2018-07-30 11:03:32 +0100

    test
    Signed-off-by: firas saadaoui <firas.saadaoui@esprit.tn>

--- src/main/java/com/example/demo/repository1/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entities.Service;
-
-public interface ServiceRepository extends JpaRepository<Service, Long>{
-
-}
