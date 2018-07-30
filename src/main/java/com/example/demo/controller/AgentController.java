4fc880aaf3eba0abaab42d5a6ef3b02f5c7ecbd5
firas saadaoui <firas.saadaoui@esprit.tn>
2018-07-30 11:59:55 +0100

    maj
    Signed-off-by: firas saadaoui <firas.saadaoui@esprit.tn>

--- src/main/java/com/example/demo/controller/AgentController.java
+++ src/main/java/com/example/demo/controller/AgentController.java
@@ -20,57 +20,70 @@ import com.example.demo.Entities.Service;
 import com.example.demo.Entities.Site;
 import com.example.demo.repository.AgentRepository;
 import com.example.demo.repository.SiteRepository;
-@CrossOrigin("*") 
+
+@CrossOrigin("*")
 @RestController
 public class AgentController {
-@Autowired
-AgentRepository r;
-@Autowired
-SiteRepository site;
-@GetMapping("/agent/afficher/{id}")
+	@Autowired
+	AgentRepository r;
+	@Autowired
+	SiteRepository site;
 
-public List<Agent> afficheragent(@PathVariable(value="id") Long id)
-{
-int i,j;	
-	List<Agent>a = new ArrayList<>();
-Site s=site.affliste(id);
-Planning p=s.getPlanning();
-List<Service>ser=p.getServices();
-for(i=0;i<ser.size();i++)
-{
-	for(j=0;j<ser.get(i).getAgents().size();j++)
-	{
-		a.add(ser.get(i).getAgents().get(j));	}
+	@GetMapping("/agent/afficher/{id}")
+
+	public List<Agent> afficheragent(@PathVariable(value = "id") Long id) {
+		int i, j;
+		List<Agent> a = new ArrayList<>();
+		Site s = site.affliste(id);
+		Planning p = s.getPlanning();
+		List<Service> ser = p.getServices();
+		for (i = 0; i < ser.size(); i++) {
+			for (j = 0; j < ser.get(i).getAgents().size(); j++) {
+				a.add(ser.get(i).getAgents().get(j));
+			}
+		}
+		return a;
 	}
-return a;
+
+	@GetMapping("/agent/affichersites")
+	public List<Site> affichersite() {
+		return site.findAll();
 	}
-@GetMapping("/agent/affichersites")
-public List<Site> affichersite()
-{
-	return site.findAll();}
 
-@PostMapping("/save")
-public Agent add(@RequestBody Agent agent) {
-	return r.save(agent);
-}
-
-@GetMapping("/getAllAgent")
-public List<Agent> getUser() {
-	return r.findAll();
-}
-
-@DeleteMapping("/delete/{id}")
-public void delete(Agent u) {
-	r.delete(u);
-
-}
-@DeleteMapping("/deleteSome")
-public void deleteSomeUser(@Valid @RequestBody List<Agent> l) {
-	for (int i =0 ; i < l.size(); i++) {
-		r.deleteById(l.get(i).getId());
+	@PostMapping("/save")
+	public Agent add(@RequestBody Agent agent) {
+		return r.save(agent);
 	}
-	// return l;
 
-}
+	@GetMapping("/getAllAgent")
+	public List<Agent> getUser() {
+		return r.findAll();
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void delete(Agent u) {
+		r.delete(u);
+
+	}
+
+	@DeleteMapping("/deleteSome")
+	public void deleteSomeUser(@Valid @RequestBody List<Agent> l) {
+		for (int i = 0; i < l.size(); i++) {
+			r.deleteById(l.get(i).getId());
+		}
+		// return l;
+
+	}
+
+	public List<Agent> getListAgentBySiteId(Long site_id) {
+		List<Site> l = site.findAll();
+		List<Agent> l3 = null;
+		for (int i = 0; i <= l.size(); i++) {
+			if (l.get(i).getId() == site_id)
+				l3 = l.get(i).getPlanning().getServices().get(i).getAgents();
+
+		}
+		return l3;
+	}
 
 }
--- src/main/java/com/example/demo/repository/SiteRepository.java
+++ src/main/java/com/example/demo/repository/SiteRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.repository.query.Param;
 import com.example.demo.Entities.Site;
 public interface SiteRepository extends JpaRepository<Site, Long> {
 @Query("select c from Site c where c.id=:id")
-
 public Site affliste(@Param("id") Long id);
 }
